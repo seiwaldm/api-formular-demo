5541f2ff780597c81473d122f76c0653559cd9df
seiwaldm <seiwald.markus@johak.at>
2022-11-29 11:18:21 +0100

    2 files modified, 1 added

---
+++ src/components/StudentList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+const studentList = ref();
+
+onMounted(async () => {
+  const response = await fetch(
+    'https://pb.seiwald.club/api/collections/4bhk/records'
+  );
+});
+</script>
+
+<template>
+  <h1>Our Students:</h1>
+</template>
